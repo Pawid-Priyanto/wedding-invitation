cd30fbc958cfacec714994a654c97037b12b69b6
Pawid-Priyanto <pawitanto27@gmail.com>
2021-10-28 22:35:35 +0700

    fix wishlist

--- src/components/WishesSection/wishlist-data.js
+++ src/components/WishesSection/wishlist-data.js
@@ -1,8 +1,29 @@
 import Wilda from '@assets/images/wilda.jpeg';
 import ariska from '@assets/images/ariska2.jpeg';
+import yusuf from '@assets/images/yusuf.jpeg';
+import Nadya from '@assets/images/nadya.jpeg';
+import Grace from '@assets/images/grace.jpeg';
+import Eka from '@assets/images/eka.jpeg';
+import Rapi from '@assets/images/rapi.jpeg';
+import Bila from '@assets/images/bila.jpeg';
+import Fitri from '@assets/images/fitri.jpeg';
+import Wulan from '@assets/images/wulan.jpeg';
+import devi from '@assets/images/devi.jpeg';
 
 
 export const wishlist = [
+  {
+    name: 'Wilda Rahmawati',
+    infoName: 'Sahabat SMA Zilda',
+    description: `Semoga menjadi keluarga SaMaWa, bahagia selalu, saling melengkapi satu sama lain dan Rumah Tangganya diberi keberkahan.. aamiin`,
+    image: Wilda,
+  },
+  {
+    name: 'M. Yusuf',
+    infoName: 'Sahabat Kuliah Pawit',
+    description: 'Jadilah suami yang tangguh dan bijaksana karena di dalam rumah tangga ada gelombang akan mengantar ke tepian atau dalam palung lautan. happy wedding sobat pawit n zilda semoga Allah memberikan keberkahan didalam pernikahan kalian aamiin.',
+    image: yusuf
+  },
   {
     name: 'Ariska',
     infoName: 'Sahabat SMP Zilda',
@@ -10,10 +31,52 @@ export const wishlist = [
     image: ariska,
   },
   {
-    name: 'Wilda Rahmawati',
+    name: 'Eka',
     infoName: 'Sahabat Kuliah Zilda',
-    description: `Semoga menjadi keluarga SaMaWa, bahagia selalu, saling melengkapi satu sama lain dan Rumah Tangganya diberi keberkahan.. aamiin`,
-    image: Wilda,
+    description: "Barakallahu laka wa baraka alaika wa jamaa baina kuma fii khoir. Semoga menjadi keluarga yang sakinah mawaddah warohmah ya zildah my best friend dan suami.  Aamiin ❤❤",
+    image: Eka,
   },
+  {
+    name: 'Bila',
+    infoName: 'Sahabat Kuliah Zilda',
+    description: "Zildah & Pawit Semoga Allah melimpahkan rahmat dan keberkahaan untuk kalian, mendapatkan keturunan yg sholeh & sholehah serta bahagia dunia akhirat Aamiin.. 🥰😘 Be happy ever after! ",
+    image: Bila,
+  },
+  {
+    name: 'Grace',
+    infoName: 'Sahabat Kuliah Zilda',
+    description: 'Selamat Menempuh Hidup Baru untuk Zildah dan Suami. Semoga menjadi keluarga yang sakinah mawaddah warahmah. Diberikan semakin banyak kebahagiaan. Aamiin',
+    image: Grace,
+  },
+  {
+    name: 'Fitri',
+    infoName: 'Sahabat SMA Zilda',
+    description: 'HAPPY WEDDING ZILDA❤️‍🔥 semoga kehidupan baru yang akan dijalanin nanti asik yah',
+    image: Fitri,
+  },
+  {
+    name: 'Nadya',
+    infoName: 'Rekan Kerja Zilda',
+    description: "Selamat menempuh hidup baru, Kak! Semoga kalian akan terus bersama hingga akhir hayat dan dipertemukan kembali di dalam surga-Nya kelak, menjadi keluarga yang sakinah, mawaddah, warahmah. Aamiin Yaa Rabbal'alamin",
+    image: Nadya,
+  },
+  {
+    name: 'Rapi',
+    infoName: 'Sahabat SMA Zilda',
+    description: 'Selamat ceu, Lancar sampai hari H dan dimudahkan segala urusannya. Semoga kalian bisa jadi pasangan yang saling mengerti satu sama lain, pasangan yang saling menguatkan apapun masalah yang nanti akan kalian hadapin, dan juga semoga rumah tangga kalian selalu diberkahi dan dirahmati sama Allah SWT',
+    image: Rapi,
+  },
+  {
+    name: 'Wulan',
+    infoName: 'Rekan Kerja Zilda',
+    description: "Happy Wedding Barakallahu laka wa baraka alaika wa jam'a bainakuma fii khoir Zildah & Suami.Semoga menjadi keluarga yang sakinah, mawadah, dan warahmah.",
+    image: Wulan,
+  },
+  {
+    name: 'Devi',
+    infoName: 'Sahabat Kuliah Zilda',
+    description: 'Zil & paw selamat cepak cepak jedar 11 anak HARUSSSS fightinggg 💪🏻💪🏻💪🏻🥰',
+    image: devi,
+  }
   
 ];
